b5a08126593996b6e3728426ac36eedb418e8e02
Arushi Tandon <arushitandon007@gmail.com>
2025-04-06 19:12:27 +0530

    Add CORS middleware to enable cross-origin requests

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const sequelize = require('./util/db');
 const passport = require('./middlewares/auth');
+const cors = require('cors');
 require('dotenv').config();
 
 
@@ -17,6 +18,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
